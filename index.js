8270f7624c3d15681d612706536d739397220fc0
Nathan Stephens <nathan.yukio.stephens@gmail.com>
2022-04-15 12:33:14 -0700

    got tag parameter to work

--- index.js
+++ index.js
@@ -11,30 +11,47 @@ const port = process.env.PORT || 3001
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
 app.get('/', (req, res) => {
     res.send("Nate's Hatchways Assessment");
 });
 
-app.get('/api/posts', (req, res) => {
+app.get('/api/ping', (req, res) => {
     request('https://api.hatchways.io/assessment/blog/posts?tag=tech', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            res.send(body)
-        } else
-
-            console.log(error);
-    });
-
-});
-
-app.get('/api/ping', (req, res) => {
-        if (response.statusCode == 200) {
             console.log(response.statusCode);
             res.json({ "success": true, "Response Status Code": response.statusCode });
         } else
-        console.log(response.statusCode, ReferenceError.prototype.message);
-
-
+            console.log(response.statusCode, error);
+    });
 });
+
+app.get('/api/posts/:tag', (req, res) => {
+    const { tag } = req.params;
+    request(`https://api.hatchways.io/assessment/blog/posts?tag=${tag}`, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body)
+        } else
+            console.log(error);
+    });
+});
+
+
+
+// trying to see if I can get post by ID
+
+// app.get('/api/:id', (req, res) => {
+//     const { id } = req.params;
+
+//     request('https://api.hatchways.io/assessment/blog/posts?:id', function (error, response, body) {
+
+//         const posts = posts.filter((post) => post.id)[0];
+//         if (!error && response.statusCode == 200) {
+//             res.send(body);
+//         } else
+//             console.log(response.statusCode, error);
+//     });
+// });
 
 
 
