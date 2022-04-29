a20925480196b17ddb23d87f5ae2768e33577966
Nathan Stephens <nathan.yukio.stephens@gmail.com>
2022-04-29 12:13:50 -0700

    sortBy endpioint works!

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
     res.send("Nate's Hatchways Assessment");
 });
 
+// use ping endpoint to check database connection
 app.get('/api/ping', (req, res) => {
     request('https://api.hatchways.io/assessment/blog/posts?tag=tech', function (error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -27,67 +28,18 @@ app.get('/api/ping', (req, res) => {
 });
 
 
-// ACTUAL SINGLE TAG ENDPOINT
-// app.get('/api/posts/:tag', (req, res) => {
-//     const { tag } = req.params;
-//     request(`https://api.hatchways.io/assessment/blog/posts?tag=${tag}`, function (error, response, body) {
-//         if (!error && response.statusCode == 200) {
-//             // returns raw response data
-//             let data = JSON.parse(body);
-//             res.send(data.posts);
-//         } else
-//             console.log(error);      
-//     });
-// });
-
-
-// TESTING SORT METHOD
+//SINGLE TAG ENDPOINT
 app.get('/api/posts/:tag', (req, res) => {
-    let arr = [
-        {
-            firstName: "Steve",
-            lastName: "Hancock",
-            score: 90
-        },
-        {
-            firstName: "Lynette",
-            lastName: "Jorgenson",
-            score: 100
-        },
-        {
-            firstName: "Andrew",
-            lastName: "Wilson",
-            score: 71
-        },
-        {
-            firstName: "Annika",
-            lastName: "Turner",
-            score: 82
-        }
-    ];
-
-    // sort by lastname
-    // arr.sort(function(a, b) {
-    //     if (a.lastName.toLowerCase() < b.lastName.toLowerCase()) 
-    //     return -1;
-
-    //     if (a.lastName.toLowerCase() > b.lastName.toLowerCase()) 
-    //     return 1;
-
-    //     if (a.lastName.toLowerCase() = b.lastName.toLowerCase()) 
-    //     return 0;
-    // });
-
-
-    // sort by score
-    arr.sort(function(a, b) {
-        return a.score - b.score;
+    const { tag } = req.params;
+    request(`https://api.hatchways.io/assessment/blog/posts?tag=${tag}`, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // returns raw response data
+            let data = JSON.parse(body);
+            res.send(data.posts);
+        } else
+            console.log(error);
     });
-
-    res.send(arr);
 });
-
-
 
 
 // TRY TO GET MULTIPLE TAGS (PERHAPS USING res.json like above?  see /ping)
@@ -115,7 +67,7 @@ app.get('/api/posts/:tags', (req, res) => {
 });
 
 
-// TRYING TO GET MULTIPLE PARAMETERS WORKING BUT IT SEEMS TO ONLY BE ACCEPTING FIRST PARAM
+// sortBy endpoint
 app.get('/api/posts/:tag/:sortBy', (req, res) => {
     const tag = req.params.tag;
     const sortBy = req.params.sortBy;
@@ -124,12 +76,50 @@ app.get('/api/posts/:tag/:sortBy', (req, res) => {
         if (!error && response.statusCode == 200) {
             // returns raw response data
             let data = JSON.parse(body);
-            res.send(data.posts);
+            let dataArr = data.posts;
+
+            // sort by id
+            if (sortBy === "id") {
+                dataArr.sort(function (a, b) {
+                    return a.id - b.id;
+                });
+
+                res.send(dataArr);
+            } else
+
+                // sort by reads
+                if (sortBy === "reads") {
+                    dataArr.sort(function (a, b) {
+                        return a.reads - b.reads;
+                    });
+
+                    res.send(dataArr);
+                } else
+
+                    // sort by likes
+                    if (sortBy === "likes") {
+                        dataArr.sort(function (a, b) {
+                            return a.likes - b.likes;
+                        });
+
+                        res.send(dataArr);
+                    } else
+
+                        // sort by popularity
+                        if (sortBy === "popularity") {
+                            dataArr.sort(function (a, b) {
+                                return a.popularity - b.popularity;
+                            });
+
+                            res.send(dataArr);
+                        }
+
         } else
             console.log(error);
     });
 });
 
+// direction endpoint
 app.get('/api/posts/:tag/:direction', (req, res) => {
     const tag = req.params.tag;
     const direction = req.params.direction;
@@ -145,11 +135,59 @@ app.get('/api/posts/:tag/:direction', (req, res) => {
     });
 });
 
-// app.use('/', require('./routes/tags'));
-
 
 
 app.listen(port, () => {
     // remember: backticks means you can insert variables with the ${syntax}
     console.log(`server is live on port: ${port}`);
 });
+
+
+// NOTES
+
+
+// TESTING SORT METHOD
+// app.get('/api/posts/:tag', (req, res) => {
+//     let arr = [
+//         {
+//             firstName: "Steve",
+//             lastName: "Hancock",
+//             score: 90
+//         },
+//         {
+//             firstName: "Lynette",
+//             lastName: "Jorgenson",
+//             score: 100
+//         },
+//         {
+//             firstName: "Andrew",
+//             lastName: "Wilson",
+//             score: 71
+//         },
+//         {
+//             firstName: "Annika",
+//             lastName: "Turner",
+//             score: 82
+//         }
+//     ];
+
+//     // sort by lastname
+//     // arr.sort(function(a, b) {
+//     //     if (a.lastName.toLowerCase() < b.lastName.toLowerCase()) 
+//     //     return -1;
+
+//     //     if (a.lastName.toLowerCase() > b.lastName.toLowerCase()) 
+//     //     return 1;
+
+//     //     if (a.lastName.toLowerCase() = b.lastName.toLowerCase()) 
+//     //     return 0;
+//     // });
+
+
+//     // sort by score
+//     arr.sort(function(a, b) {
+//         return a.score - b.score;
+//     });
+
+//     res.send(arr);
+// });
